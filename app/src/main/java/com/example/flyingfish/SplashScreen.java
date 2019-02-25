2db7505bcee7762d46fe20162bbbf78c4fb3ccb2
RanbirKaushik <ranbirsharma127033@gmail.com>
2019-02-25 12:02:26 +0530

    v1.0.0

---
+++ app/src/main/java/com/example/flyingfish/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.example.flyingfish;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+
+            public void run(){
+                try{
+                    sleep(5000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
